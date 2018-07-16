91d0b8aacc5b39812d513f7eae72206d2a7fbc6e
DutchMon <dutch.holland13@gmail.com>
2018-07-16 14:04:08 -0700

    Displaying cards is determined by the dropdown menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,7 +28,7 @@ var img;
       $('.show').removeClass("wrapper-hidden")
       $cardsInfo = $("#cardsInfo > tbody");
   
-      console.log(response)
+      console.log(response);
 
       var setNamesArray = [];
 
@@ -38,17 +38,10 @@ var img;
         }
       }
 
-
-      
-      
-
       console.log(setNamesArray);
       console.log(setNamesArray[0]);
       console.log(response.Basic.length);
-      
-
-      
-
+     
 
 
       for (var i = 0; i < setNamesArray.length; i++) {
@@ -58,11 +51,44 @@ var img;
       }
       
 
-      $('.displayCards').on('click',function(){
+      $('.displayCards').on('click',function(replace){
 
-          console.log("Hey")
-      
+        $('.cardsDisplay').empty();
+
+        var replace = $(this).text();
+
+        for (var i = 0; i < response[replace].length; i++) {
+
+          var card = response[replace][i];
         
+          var $checkBox = $("<input class='selector' type='checkbox' value=" + card.name + ">");
+          var img = card.img;
+          var imgElement = $("<img class='cardImage' src=" + img + " alt=img>");
+          var text = card.text
+  
+          //if array has a img print the image to the page, otherwise print no image avaiable
+          if (img === undefined) {
+            //changes imgElement to write No Image Available to the table
+            imgElement = "No Image Available"
+          }
+          //does the same things as the top one for text
+          if (text === undefined) {
+            text = "No text available."
+          }
+          
+          var newRow = $("<tr>").append(
+            $("<td>").prepend($checkBox).append(imgElement),
+            $("<td>").text(card.name),
+            $("<td>").text(text),
+          );
+          $cardsInfo.append(newRow);
+      
+          $("body").css("background-color", "#fff");
+          $("#cardsInfo").fadeIn(200);
+        };
+        
+        
+    
       });
 
       //for the length of the information revieved from the basic deck
@@ -70,63 +96,7 @@ var img;
 
       
 
-<<<<<<< HEAD
-      // for (var i = 0; i < response.Basic.length; i++) {
-
-      //   var img = response.Basic[i].img;
-      //   var imgElement = $("<img class='cardImage' src=" + img + " alt=img>");
-      //   var text = response.Basic[i].text
-
-      //   //if array has a img print the image to the page, otherwise print no image avaiable
-      //   if (img === undefined) {
-      //     //changes imgElement to write No Image Available to the table
-      //     imgElement = "No Image Available"
-      //   }
-      //   //does the same things as the top one for text
-      //   if (text === undefined) {
-      //     text = "No text available."
-      //   }
-        
-      //   var newRow = $("<tr>").append(
-      //     $("<td>").append(imgElement),
-      //     $("<td>").text(response.Basic[i].name),
-      //     $("<td>").text(text),
-      //   );
-      //   $cardsInfo.append(newRow);
-    
-      //   $("body").css("background-color", "#fff");
-      //   $("#cardsInfo").fadeIn(200);
-      //   console.log("You clicked the cards Info button!");
-      // };
-=======
-      for (var i = 0; i < response.Basic.length; i++) {
-        
-        var $checkBox = $("<input class='selector' type='checkbox' value=" + response.Basic[i].name + ">");
-        var img = response.Basic[i].img;
-        var imgElement = $("<img class='cardImage' src=" + img + " alt=img>");
-        var text = response.Basic[i].text
-
-        //if array has a img print the image to the page, otherwise print no image avaiable
-        if (img === undefined) {
-          //changes imgElement to write No Image Available to the table
-          imgElement = "No Image Available"
-        }
-        //does the same things as the top one for text
-        if (text === undefined) {
-          text = "No text available."
-        }
-        
-        var newRow = $("<tr>").append(
-          $("<td>").prepend($checkBox).append(imgElement),
-          $("<td>").text(response.Basic[i].name),
-          $("<td>").text(text),
-        );
-        $cardsInfo.append(newRow);
-    
-        $("body").css("background-color", "#fff");
-        $("#cardsInfo").fadeIn(200);
-        console.log("You clicked the cards Info button!");
-      };
+      
 
       $makeDeck.on("click", function() {
         $playerDeck = $("#playerDeck > tbody");
@@ -153,10 +123,6 @@ var img;
     
         }
       })
->>>>>>> b679c1ebcf2a420c58bc8137572aa4d2f775f0ec
     });
   });
 
-  
-
-});
