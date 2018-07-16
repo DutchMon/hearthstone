01a7b462da875c01ac4ed25b82adabf6a577a6a2
DutchMon <dutch.holland13@gmail.com>
2018-07-16 13:13:56 -0700

    small change

--- assets/js/app.js
+++ assets/js/app.js
@@ -63,52 +63,52 @@ $(document).ready(function() {
 
 
 
-      // for (var i = 0; i < setNamesArray.length; i++) {
+      for (var i = 0; i < setNamesArray.length; i++) {
 
-      // $('.dropdown-menu').append("<a class= dropdown-item>"+setNamesArray[i]+"</a>")
-      // $('.dropdown-menu').addClass("displayCards");
-      // }
+      $('.dropdown-menu').append("<a class= dropdown-item>"+setNamesArray[i]+"</a>")
+      $('a').addClass("displayCards");
+      }
       
 
-      // $('.displayCards').on('click',function(){
+      $('.displayCards').on('click',function(){
 
-      //     console.log("Hey")
+          console.log("Hey")
       
         
-      // });
+      });
 
       //for the length of the information revieved from the basic deck
       //need to recieve the length of the deck of cards,  not from jsut the basic deck
 
       
 
-      for (var i = 0; i < response.Basic.length; i++) {
+      // for (var i = 0; i < response.Basic.length; i++) {
 
-        var img = response.Basic[i].img;
-        var imgElement = $("<img class='cardImage' src=" + img + " alt=img>");
-        var text = response.Basic[i].text
+      //   var img = response.Basic[i].img;
+      //   var imgElement = $("<img class='cardImage' src=" + img + " alt=img>");
+      //   var text = response.Basic[i].text
 
-        //if array has a img print the image to the page, otherwise print no image avaiable
-        if (img === undefined) {
-          //changes imgElement to write No Image Available to the table
-          imgElement = "No Image Available"
-        }
-        //does the same things as the top one for text
-        if (text === undefined) {
-          text = "No text available."
-        }
+      //   //if array has a img print the image to the page, otherwise print no image avaiable
+      //   if (img === undefined) {
+      //     //changes imgElement to write No Image Available to the table
+      //     imgElement = "No Image Available"
+      //   }
+      //   //does the same things as the top one for text
+      //   if (text === undefined) {
+      //     text = "No text available."
+      //   }
         
-        var newRow = $("<tr>").append(
-          $("<td>").append(imgElement),
-          $("<td>").text(response.Basic[i].name),
-          $("<td>").text(text),
-        );
-        $cardsInfo.append(newRow);
+      //   var newRow = $("<tr>").append(
+      //     $("<td>").append(imgElement),
+      //     $("<td>").text(response.Basic[i].name),
+      //     $("<td>").text(text),
+      //   );
+      //   $cardsInfo.append(newRow);
     
-        $("body").css("background-color", "#fff");
-        $("#cardsInfo").fadeIn(200);
-        console.log("You clicked the cards Info button!");
-      };
+      //   $("body").css("background-color", "#fff");
+      //   $("#cardsInfo").fadeIn(200);
+      //   console.log("You clicked the cards Info button!");
+      // };
     });
   });
 
